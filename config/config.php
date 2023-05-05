7ee5290293860d83acdcde0383626074c1481736
Anderson Souza <andersonsouza007@live.com>
2023-05-05 11:29:45 -0300

    first commit

---
+++ config/config.php
@@ -0,0 +1,20 @@
+<?php
+
+
+// define("SERVIDOR", "26.251.9.111");
+// define("BANCO", "bd_presencas");
+// define("USUARIO", "prepara2");
+// define("SENHA", "prepara");
+
+define('CONTROLLER_PADRAO', 'home');
+define('METODO_PADRAO', 'index');
+define('NAMESPACE_CONTROLLER', 'app\\controllers\\');
+define("PASTA_PADRAO", "app/controllers/");
+define('URL_BASE', 'http://localhost/aya/');
+define("SESSION_LOGIN", "logado");
+define('ROOT', dirname(__FILE__, 3));
+define('CONTROLLER_PATH', 'app/controllers/');
+define('CONTROLLER_DEFAULT', 'Home');
+define('FOLDER_DEFAULT', 'site');
+// $bloqueios = isset($_SESSION[SESSION_LOGIN]) ? $_SESSION[SESSION_LOGIN]["Bloqueios"]:[];
+// define("PERMISSOES", $bloqueios);
